a9c43fd5ae24d75551ad8f4568ed17feca3f4436
danielTLevy <danieltlevy@gmail.com>
2021-09-27 22:36:29 -0400

    Fix previous commit

--- scripts/hgb/NC/run.py
+++ scripts/hgb/NC/run.py
@@ -248,11 +248,11 @@ Val Macro-F1: {:.3f}".format(val_loss, val_micro, val_macro))
             else:
                 pred = test_logits.cpu().numpy().argmax(axis=1)
                 onehot = np.eye(num_classes, dtype=np.int32)
-                if args.wandb_log_run:
-                    file_path = f"test_out/{args.dataset}_{run_name}.txt"
-                else:
-                    file_path = f"test_out/{args.dataset}_{run_name}.txt"
 
+            if args.wandb_log_run:
+                file_path = f"test_out/{args.dataset}_{run_name}.txt"
+            else:
+                file_path = f"test_out/{args.dataset}_{run_name}.txt"
             dl.gen_file_for_evaluate(test_idx=test_idx, label=pred,
                                      file_path=file_path,
                                      multi_label=args.multi_label)

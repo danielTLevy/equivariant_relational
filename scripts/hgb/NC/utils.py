94bf868241acc6d47577878ad930ade461a8fee0
danielTLevy <danieltlevy@gmail.com>
2022-03-10 13:21:58 -0500

    NC: Unify hyperparameters between script files

--- scripts/hgb/NC/utils.py
+++ scripts/hgb/NC/utils.py
@@ -2,6 +2,110 @@
 
 import numpy as np
 import torch
+import argparse
+
+def get_hyperparams(argv):
+    ap = argparse.ArgumentParser(allow_abbrev=False,
+                                 description='EquivHGN for Node Classification')
+    ap.set_defaults(dataset='PubMed')
+    ap.add_argument('--feats_type', type=int, default=0,
+                    help='Type of the node features used. ' +
+                         '0 - loaded features; ' +
+                         '1 - only target node features (zero vec for others); ' +
+                         '2 - only target node features (id vec for others); ' +
+                         '3 - all id vec. Default is 2;' +
+                        '4 - only term features (id vec for others);' + 
+                        '5 - only term features (zero vec for others).')
+    ap.add_argument('--epoch', type=int, default=300, help='Number of epochs.')
+    ap.add_argument('--patience', type=int, default=30, help='Patience.')
+    ap.add_argument('--repeat', type=int, default=1,
+                    help='Repeat the training and testing for N times. Default is 1.')
+    ap.add_argument('--lr', type=float, default=1e-3)
+    ap.add_argument('--dropout', type=float, default=0.5)
+    ap.add_argument('--dataset', type=str, default='IMDB')
+    ap.add_argument('--checkpoint_path', type=str, default='')
+    ap.add_argument('--width', type=int, default=64)
+    ap.add_argument('--depth', type=int, default=3)
+    ap.add_argument('--fc_layer', type=int, default=64)
+    ap.add_argument('--weight_decay', type=float, default=1e-4)
+    ap.add_argument('--act_fn', type=str, default='LeakyReLU')
+    ap.add_argument('--in_fc_layer', type=int, default=1)
+    ap.add_argument('--mid_fc_layer', type=int, default=0)
+    ap.add_argument('--optimizer', type=str, default='Adam')
+    ap.add_argument('--val_every', type=int, default=5)
+    ap.add_argument('--seed', type=int, default=1)
+    ap.add_argument('--norm',  type=int, default=1)
+    ap.add_argument('--norm_affine', type=int, default=1)
+    ap.add_argument('--norm_out', action='store_true', default=False)
+    ap.add_argument('--pool_op', type=str, default='mean')
+    ap.add_argument('--use_edge_data',  type=int, default=1)
+    ap.add_argument('--use_node_attr',  type=int, default=1)
+    ap.add_argument('--residual', action='store_true', default=False)
+    ap.add_argument('--save_embeddings', dest='save_embeddings',
+                    action='store_true', default=True)
+    ap.add_argument('--no_save_embeddings', dest='save_embeddings',
+                    action='store_false', default=True)
+    ap.set_defaults(save_embeddings=True)
+    ap.add_argument('--wandb_log_param_freq', type=int, default=1000)
+    ap.add_argument('--wandb_log_loss_freq', type=int, default=1)
+    ap.add_argument('--wandb_log_run', dest='wandb_log_run', action='store_true',
+                        help='Log this run in wandb')
+    ap.add_argument('--wandb_no_log_run', dest='wandb_log_run', action='store_false',
+                        help='Do not log this run in wandb')
+    ap.add_argument('--output', type=str)
+    ap.add_argument('--run', type=int, default=1)
+    ap.add_argument('--multi_label', default=False, action='store_true',
+                    help='multi-label classification. Only valid for IMDb dataset')
+    ap.add_argument('--evaluate', type=int, default=1)
+    ap.add_argument('--lgnn', action='store_true', default=False)
+    ap.add_argument("--removed_params", type=int, nargs='*', default=None)
+    ap.add_argument("--asymmetric", action='store_true', default=False)
+    ap.add_argument('--alternating', action='store_true', default=False)
+    ap.add_argument('--embedding_dim', type=int, default=32)
+    ap.set_defaults(wandb_log_run=False)
+
+    args, argv = ap.parse_known_args(argv)
+
+    if args.output == None:
+        args.output = args.dataset + '_emb.dat'
+    if args.dataset == 'IMDB':
+        args.multi_label = True
+    if args.in_fc_layer == 1:
+        args.in_fc_layer = True
+    else:
+        args.in_fc_layer = False
+    if args.mid_fc_layer == 1:
+        args.mid_fc_layer = True
+    else:
+        args.mid_fc_layer = False
+    if args.evaluate == 1:
+        args.evaluate = True
+    else:
+        args.evaluate = False
+    if args.norm_affine == 1:
+        args.norm_affine = True
+    else:
+        args.norm_affine = False
+    if args.norm == 1:
+        args.norm = True
+    else:
+        args.norm = False
+    if args.use_edge_data == 1:
+        args.use_edge_data = True
+    else:
+        args.use_edge_data = False
+    if args.use_node_attr == 1:
+        args.use_node_attr = True
+    else:
+        args.use_node_attr = False
+    args.layers = [args.width]*args.depth
+    if args.fc_layer == 0:
+        args.fc_layers = []
+    else:
+        args.fc_layers = [args.fc_layer]
+
+    return args
+
 
 
 class EarlyStopping:
